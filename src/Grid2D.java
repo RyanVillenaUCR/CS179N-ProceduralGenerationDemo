a6821a54e63fbe6757d972884e20b954c41e191f
RyanVillenaUCR <rvill028@ucr.edu>
2019-04-23 15:49:14 -0700

    Slightly improved efficiency of toString()

--- src/Grid2D.java
+++ src/Grid2D.java
@@ -30,7 +30,7 @@ public class Grid2D {
     @Override
     public String toString() {
         
-        StringBuilder sb = new StringBuilder();
+        StringBuilder sb = new StringBuilder((ROWS + 2) * (COLS + 2));
         
         // Top row border
         sb.append('*');
