2ce74909ca23471a68efd3410b5e15091219edaf
RyanVillenaUCR <rvill028@ucr.edu>
2019-04-25 00:38:43 -0700

    Added mark functionality

--- src/Tile.java
+++ src/Tile.java
@@ -1,10 +1,25 @@
 /**
- * @brief Enum used to represent tiles
+ * Enum used to represent tiles, which can be marked or unmarked.
  * @author Ryan Villena
  *
  */
 public enum Tile {
+    
     EMPTY,
     TRAVERSABLE,
-    NON_TRAVERSABLE
+    NON_TRAVERSABLE;
+
+    Tile() {
+        this.marked = false;
+    }
+    
+    public boolean isMarked() {
+        return marked;
+    }
+    
+    public void setMark(boolean mark) {
+        this.marked = mark;
+    }
+    
+    private boolean marked;
 }
