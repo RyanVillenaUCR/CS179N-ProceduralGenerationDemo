08cd6f74348ab34aec7c713da7ebf657c38d8665
RyanVillenaUCR <rvill028@ucr.edu>
2019-04-25 01:08:57 -0700

    Changed Tile implementation
    * Before, Tile was an enum
    * This wasn't great because we wanted each Tile object to know information about itself,
      but Tile enum objects behaved as static references to the _same_ object
      - In other words, we couldn't instantiate new Tile objects using the enum implementation
    * Tile is now its own concrete class
    * Tile.TileType is an enclosed enum which defines the different types
    * A tile can be any combination of marked/unmarked and any different type
    * Also updated with a fresh new toString() override

--- src/Tile.java
+++ src/Tile.java
@@ -3,14 +3,34 @@
  * @author Ryan Villena
  *
  */
-public enum Tile {
+public class Tile {
     
-    EMPTY,
-    TRAVERSABLE,
-    NON_TRAVERSABLE;
+    public enum TileType {
+        EMPTY,
+        TRAVERSABLE,
+        NON_TRAVERSABLE
+    }
 
     Tile() {
         this.marked = false;
+        this.type = TileType.EMPTY;
+    }
+
+    Tile(TileType type) {
+        
+        this.marked = false;
+        this.type = type;
+    }
+    
+    Tile(TileType type, boolean mark) {
+        
+        this.marked = mark;
+        this.type = type;
+    }
+    
+    Tile(Tile t) {
+        this.marked = t.marked;
+        this.type = t.type;
     }
     
     public boolean isMarked() {
@@ -21,5 +41,18 @@ public enum Tile {
         this.marked = mark;
     }
     
+    public TileType getType() {
+        
+        return type;
+    }
+    
+    @Override
+    public String toString() {
+        
+        return TileType.class.getSimpleName() + " " + this.type.toString() + ", "
+                + (marked ? "" : "not ") + "marked";
+    }
+    
     private boolean marked;
+    private TileType type;
 }
