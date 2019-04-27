fcae6f6fc75694584a0729ffcad7887a03ba242d
RyanVillenaUCR <rvill028@ucr.edu>
2019-04-27 02:30:33 -0700

    Added distance field for use in Dijkstra's algorithm

--- src/Tile.java
+++ src/Tile.java
@@ -1,5 +1,5 @@
 /**
- * Enum used to represent tiles, which can be marked or unmarked.
+ * Class used to represent tiles, which can be marked or unmarked.
  * @author Ryan Villena
  *
  */
@@ -14,26 +14,40 @@ public class Tile {
     Tile() {
         this.marked = false;
         this.type = TileType.EMPTY;
+        this.distance = Integer.MAX_VALUE;
     }
 
     Tile(TileType type) {
         
         this.marked = false;
         this.type = type;
+        this.distance = Integer.MAX_VALUE;
+    }
+    
+    Tile(TileType type, int distance) {
+        
+        this.marked = false;
+        this.type = type;
+        this.distance = distance;
     }
     
     Tile(TileType type, boolean mark) {
         
         this.marked = mark;
         this.type = type;
+        this.distance = Integer.MAX_VALUE;
     }
     
     Tile(Tile t) {
         this.marked = t.marked;
         this.type = t.type;
+        this.distance = Integer.MAX_VALUE;
     }
     
     public char getChar() {
+        
+        if (isMarked())
+            return '*';
         
         switch (type) {
             
@@ -56,6 +70,11 @@ public class Tile {
         this.marked = mark;
     }
     
+    public void setType(TileType t) {
+        
+        this.type = t;
+    }
+    
     public TileType getType() {
         
         return type;
@@ -65,9 +84,21 @@ public class Tile {
     public String toString() {
         
         return TileType.class.getSimpleName() + " " + this.type.toString() + ", "
-                + (marked ? "" : "not ") + "marked";
+                + (marked ? "" : "not ") + "marked, "
+                + "distance = " + (distance == Integer.MAX_VALUE ? "MAX_VALUE" : distance);
+    }
+    
+    public void setDistance(int distance) {
+        
+        this.distance = distance;
+    }
+    
+    public int getDistance() {
+        
+        return distance;
     }
     
     private boolean marked;
     private TileType type;
+    private int distance;
 }
