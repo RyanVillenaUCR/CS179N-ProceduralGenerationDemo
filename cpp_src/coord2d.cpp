1394888ec3f5bbedef6104e069d763bebd3896eb
RyanVillenaUCR <rvill028@ucr.edu>
2019-06-06 03:45:46 -0700

    Can check if coords lie on same line

--- cpp_src/coord2d.cpp
+++ cpp_src/coord2d.cpp
@@ -5,3 +5,18 @@ std::ostream& operator <<(std::ostream& out, const Coord2D& printMe) {
     out << "(" << printMe.first << ", " << printMe.second << ")";
     return out;
 }
+
+bool areSameRow(const Coord2D& point1, const Coord2D& point2) {
+
+    return point1.second == point2.second;
+}
+
+bool areSameCol(const Coord2D& point1, const Coord2D& point2) {
+
+    return point1.first == point2.first;
+}
+
+bool areOnSameLine(const Coord2D& point1, const Coord2D& point2) {
+
+    return areSameRow(point1, point2) || areSameCol(point1, point2);
+}
--- cpp_src/coord2d.hpp
+++ cpp_src/coord2d.hpp
@@ -9,5 +9,9 @@
 //std::ostream& printCoord2D(std::ostream& out, const Coord2D& printMe);
 std::ostream& operator <<(std::ostream& out, const Coord2D& printMe);
 
+bool areSameRow(const Coord2D& point1, const Coord2D& point2);
+bool areSameCol(const Coord2D& point1, const Coord2D& point2);
+bool areOnSameLine(const Coord2D& point1, const Coord2D& point2);
+
 
 #endif // COORD2D_HPP
